0e331d09f30a1c582c506de38c4e9093b7fee323
Prashant <prashant001y@gmail.com>
2024-06-12 01:57:24 +0530

    2 commint

--- Data/database.js
+++ Data/database.js
@@ -2,8 +2,8 @@
 import mongoose from "mongoose";
 export const  databaseDB=()=>{
     
-    mongoose.connect(process.env.MONGO_URI,{
+mongoose.connect(process.env.MONGO_URI,{
         dbName:"fullStackTodo"
-    }).then(()=>console.log("connection with mongo db successful")).catch((e)=>console.log(e));
+    }).then((c)=>console.log(`Database connected with ${c.connection.host}`)).catch((e)=>console.log(e));
 
 }
